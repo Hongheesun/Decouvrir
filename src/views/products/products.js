8e288ff1d8e22b41c3ac053c25776203a45d8d05
홍희선 <hia990618@gmail.com>
2022-11-10 16:43:32 +0900

    feat : products 페이지

--- src/views/products/products.js
+++ src/views/products/products.js
@@ -8,168 +8,41 @@ async function getProducts() {
   }
 }
 
-// async function sameRenderPage(category,categoryWrapper){
-//   let products = await getProducts();
-//   const abstract = "6368f390b4a8016623514ea2";
-//   const landscape = "6368f39db4a8016623514ea6";
-//   const illustraction = "6368f3abb4a8016623514eae";
-//   const asian = "6368f3b0b4a8016623514eb2";
-//   const arr = [];
-//   console.log(products);
-//   let html = "";
-//   products.forEach((data) => {
-//     if (data["categoryId"] == category) {
-//       arr.push(data);
-//     }
-//   });
-//   for (let i = 0; i < 4; i++) {
-//     let htmlSegment = `
-//     <li class="work">
-//       <div class="product-image"></div>
-//       <span class="work-info">
-//           <span>${arr[i].painterName} | ${arr[i].productName}
-//           </span>
-//       </span>
-//       <span class="price">${arr[i].price}</span>
-//     </li>`;
+const abstract = "6368f390b4a8016623514ea2";
+const landscape = "6368f39db4a8016623514ea6";
+const illustration = "6368f3abb4a8016623514eae";
+const asian = "6368f3b0b4a8016623514eb2";
 
-//     html += htmlSegment;
-//   }
-
-//   let container = document.querySelector("#abstract-work-wrapper");
-//   container.innerHTML = html;
-// }
-
-async function renderAbstractProducts() {
+async function renderProducts(category, wrapperName) {
   let products = await getProducts();
-  const abstract = "6368f390b4a8016623514ea2";
-  const landscape = "6368f39db4a8016623514ea6";
-  const illustraction = "6368f3abb4a8016623514eae";
-  const asian = "6368f3b0b4a8016623514eb2";
-  const abstractArr = [];
+  const arr = [];
   console.log(products);
   let html = "";
   products.forEach((data) => {
-    if (data["categoryId"] == abstract) {
-      abstractArr.push(data);
+    if (data["categoryId"] == category) {
+      arr.push(data);
     }
+    console.log(arr);
   });
   for (let i = 0; i < 4; i++) {
     let htmlSegment = `   
     <li class="work">
       <div class="product-image"></div>
       <span class="work-info">
-          <span>${abstractArr[i].painterName} | ${abstractArr[i].productName}
+          <span>${arr[i].painterName} | ${arr[i].productName}
           </span>
       </span>
-      <span class="price">${abstractArr[i].price}</span>
+      <span class="price">${arr[i].price}</span>
     </li>`;
 
     html += htmlSegment;
   }
 
-  let container = document.querySelector("#abstract-work-wrapper");
+  let container = document.querySelector(wrapperName);
   container.innerHTML = html;
 }
 
-renderAbstractProducts();
-
-async function renderAbstractProducts() {
-  let products = await getProducts();
-  const abstract = "6368f390b4a8016623514ea2";
-  const landscape = "6368f39db4a8016623514ea6";
-  const illustraction = "6368f3abb4a8016623514eae";
-  const asian = "6368f3b0b4a8016623514eb2";
-  const abstractArr = [];
-  console.log(products);
-  let html = "";
-  products.forEach((data) => {
-    if (data["categoryId"] == abstract) {
-      abstractArr.push(data);
-    }
-  });
-  for (let i = 0; i < 4; i++) {
-    let htmlSegment = `   
-    <li class="work">
-      <div class="product-image"></div>
-      <span class="work-info">
-          <span>${abstractArr[i].painterName} | ${abstractArr[i].productName}
-          </span>
-      </span>
-      <span class="price">${abstractArr[i].price}</span>
-    </li>`;
-
-    html += htmlSegment;
-  }
-
-  let container = document.querySelector("#abstract-work-wrapper");
-  container.innerHTML = html;
-}
-
-renderAbstractProducts();
-async function renderAbstractProducts() {
-  let products = await getProducts();
-  const abstract = "6368f390b4a8016623514ea2";
-  const landscape = "6368f39db4a8016623514ea6";
-  const illustraction = "6368f3abb4a8016623514eae";
-  const asian = "6368f3b0b4a8016623514eb2";
-  const abstractArr = [];
-  console.log(products);
-  let html = "";
-  products.forEach((data) => {
-    if (data["categoryId"] == abstract) {
-      abstractArr.push(data);
-    }
-  });
-  for (let i = 0; i < 4; i++) {
-    let htmlSegment = `   
-    <li class="work">
-      <div class="product-image"></div>
-      <span class="work-info">
-          <span>${abstractArr[i].painterName} | ${abstractArr[i].productName}
-          </span>
-      </span>
-      <span class="price">${abstractArr[i].price}</span>
-    </li>`;
-
-    html += htmlSegment;
-  }
-
-  let container = document.querySelector("#abstract-work-wrapper");
-  container.innerHTML = html;
-}
-
-renderAbstractProducts();
-async function renderAbstractProducts() {
-  let products = await getProducts();
-  const abstract = "6368f390b4a8016623514ea2";
-  const landscape = "6368f39db4a8016623514ea6";
-  const illustraction = "6368f3abb4a8016623514eae";
-  const asian = "6368f3b0b4a8016623514eb2";
-  const abstractArr = [];
-  console.log(products);
-  let html = "";
-  products.forEach((data) => {
-    if (data["categoryId"] == abstract) {
-      abstractArr.push(data);
-    }
-  });
-  for (let i = 0; i < 4; i++) {
-    let htmlSegment = `   
-    <li class="work">
-      <div class="product-image"></div>
-      <span class="work-info">
-          <span>${abstractArr[i].painterName} | ${abstractArr[i].productName}
-          </span>
-      </span>
-      <span class="price">${abstractArr[i].price}</span>
-    </li>`;
-
-    html += htmlSegment;
-  }
-
-  let container = document.querySelector("#illustraction-work-wrapper");
-  container.innerHTML = html;
-}
-
-renderIllustractionProducts();
+renderProducts(abstract, "#abstract-work-wrapper");
+renderProducts(asian, "#asian-work-wrapper");
+renderProducts(landscape, "#landscape-work-wrapper");
+renderProducts(illustration, "#illustration-work-wrapper");
