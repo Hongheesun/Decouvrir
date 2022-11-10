bf5823c6f1a86f635587017e5d85483ca931eb52
hsj <ciko111@naver.com>
2022-11-10 16:11:59 +0900

    package npm -i --save --

--- src/middlewares/myproduct.js
+++ src/middlewares/myproduct.js
@@ -4,16 +4,6 @@ function myProduct(req, res, next) {
     const painterToken = req.headers['authorization']?.split(' ')[1];
     const painterEmail = req.body.painterEmail;
 
-    if (!painterToken || painterToken ==='null') {
-        console.log('서비스 사용 요청이 있습니다. 하지만, Authorization 토큰: 없음');
-        res.status(401).json({
-            status: 401,
-            result: 'forbidden-approach',
-            reason: '로그인한 유저만 사용할 수 있는 서비스입니다.',
-        });
-        return;
-    }
-
     try {
         const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
         const jwtDecoded = jwt.verify(painterToken, secretKey);
